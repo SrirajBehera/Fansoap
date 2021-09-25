9a5af4c17cca4337995d2c6ad678509c143fcce3
Sriraj <behera.sriraj2911@gmail.com>
2021-09-25 16:18:18 +0530

    completed input area styling

--- src/screens/LoginScreen/SignUpScreen/SignUpScreen.js
+++ src/screens/LoginScreen/SignUpScreen/SignUpScreen.js
@@ -1,41 +1,76 @@
 import React from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import { styles } from './SignUpScreen.styles';
 import ic_user from '../../../assets/images/ic_user.svg';
+import ic_email from '../../../assets/images/ic_email.svg';
+import ic_password from '../../../assets/images/ic_password.svg';
+import ic_key from '../../../assets/images/ic_key.svg';
+import ic_eye from '../../../assets/images/ic_eye.svg';
+import ic_eye_crossed from '../../../assets/images/ic_eye_crossed.svg';
 
 export function SignUpScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>FanSoap</Text>
-      <View>
-        <SvgXml xml={ic_user} />
-        <TextInput
-          style={styles.inputText}
-          placeholder="Name"
-        >
-        </TextInput>
-      </View>
-      <TextInput
-        style={styles.inputText}
-        placeholder="Email Address"
-      >
-      </TextInput>
-      <TextInput
-        style={styles.inputText}
-        placeholder="Password"
-      >
-      </TextInput>
-      <TextInput
-        style={styles.inputText}
-        placeholder="Confirm Password"
-      >
-      </TextInput>
+      <View style={styles.inputArea}>
+        <View style={styles.focusView} >
+          <TouchableOpacity
+            style={styles.placeholderIcon}>
+            <SvgXml xml={ic_user} />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Name"
+          >
+          </TextInput>
+        </View>
+        <View style={styles.focusView} >
+          <TouchableOpacity
+            style={styles.placeholderIcon}>
+            <SvgXml xml={ic_email} />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email Address"
+          >
+          </TextInput>
+        </View>
+        
+        <View style={styles.focusView} >
+          <TouchableOpacity
+            style={styles.placeholderIcon}>
+            <SvgXml xml={ic_password} />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Password"
+          >
+          </TextInput>
+          <TouchableOpacity
+            style={styles.passwordEye}>
+            <SvgXml xml={ic_eye} />
+          </TouchableOpacity>
+        </View>
 
-      <Button
-        title="SIGNUP"
+        <View style={styles.focusView} >
+          <TouchableOpacity
+            style={styles.placeholderIcon}>
+            <SvgXml xml={ic_key} />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Confirm Password"
+          >
+          </TextInput>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={styles.signUpButton}
       >
-      </Button>
+        <Text style={styles.signUpText}>SIGN UP</Text>
+      </TouchableOpacity>
 
       <Text>OR</Text>
 
--- src/screens/LoginScreen/SignUpScreen/SignUpScreen.styles.js
+++ src/screens/LoginScreen/SignUpScreen/SignUpScreen.styles.js
@@ -1,15 +1,46 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        textAlign: 'center'
-    },
-    inputText: {
-        borderColor: '#1C6E9D',
-        borderWidth: 2,
-        margin: 10,
-        padding: 10,
-        borderRadius: 7
-    }
+  container: {
+    margin: 40,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  inputArea: {
+    marginVertical: 40,
+  },
+  inputText: {
+    width: '80%',
+    paddingHorizontal: 10,
+    // borderWidth: 2,
+    marginHorizontal: 30,
+    fontSize: 16,
+  },
+  focusView: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    borderColor: '#1C6E9D',
+    borderWidth: 2,
+    borderRadius: 7,
+    margin: 10,
+  },
+  placeholderIcon: {
+    top: 12.5,
+    left: 15,
+  },
+  passwordEye: {
+    right: 60,
+    top: 12.5,
+  },
+  signUpButton: {
+    width: '50%',
+    // marginHorizontal: 50,
+    alignSelf: 'center',
+    borderWidth: 2,
+  },
+  signUpText: {
+    textAlign: 'center',
+  },
 })
